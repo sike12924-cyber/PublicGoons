d2b507228c99cf178f2adae9c1043894d623f804
sike12924-cyber <sike12924@gmail.com>
2026-04-28 19:14:13 +0200

    Add files via upload

---
+++ PublicGoon/src/main/java/org/please/publicGoon/PublicGoon.java
@@ -0,0 +1,17 @@
+package org.please.publicGoon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PublicGoon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
